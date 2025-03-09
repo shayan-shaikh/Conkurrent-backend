96dae302a762182d0c13888ee6f07a4167eac99d
shayan-shaikh <shaikhshayan1@gmail.com>
2025-03-10 02:35:03 +0530

    refactor: mongo logs

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDb = () => {
 mongoose.connect(process.env.MONGODB_URI as string)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log('MongoDB connected✅'))
+  .catch(err => console.error('MongoDB connection error🚨:', err));
 }
