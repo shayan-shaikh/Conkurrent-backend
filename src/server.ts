224e812548fa50b207da6ad4a07243fad9eb0329
shayan-shaikh <shaikhshayan1@gmail.com>
2025-03-14 02:25:00 +0530

    feat: CORS

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.UI_URL,
+  methods: 'GET,POST,PUT,DELETE,PATCH',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+
 app.use(express.json());
 
 // Connect to MongoDB
